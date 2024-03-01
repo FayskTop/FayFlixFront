1ba83b5c440e144ada8ca8db206d4ccc687344b2
Faysk <faysk.nan@gmail.com>
2024-03-01 10:48:28 -0300

    melhorias, correções, tratamentos de erros e novos endpoints

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import styles from './App.module.scss'; 
 
+const defaultThumbnail = 'https://storagefayflix.blob.core.windows.net/webpage/404.jpeg';
+const defaultGif = 'https://storagefayflix.blob.core.windows.net/webpage/404.gif';
+
 function App() {
   const [videos, setVideos] = useState([]);
-  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const [gifs, setGifs] = useState([]);
+  const [thumbnails, setThumbnails] = useState([]);
   const [showVideoPopup, setShowVideoPopup] = useState(false);
   const [popupVideoUrl, setPopupVideoUrl] = useState('');
   const [darkMode, setDarkMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    loadVideos();
+    async function fetchData() {
+      try {
+        const videosResponse = await fetchContent('videos');
+        const gifsResponse = await fetchContent('gifs');
+        const thumbnailsResponse = await fetchContent('thumbnails');
+
+        setVideos(videosResponse);
+        setGifs(gifsResponse);
+        setThumbnails(thumbnailsResponse);
+        setLoading(false);
+      } catch (error) {
+        setError('Failed to load content. Please try again later.');
+        setLoading(false);
+      }
+    }
+
+    fetchData();
   }, []);
 
-  async function loadVideos() {
-    try {
-      const response = await fetch('https://apiserverfile.azurewebsites.net/videos');
-      const videosData = await response.json();
-      setVideos(videosData);
-    } catch (error) {
-      console.error('Error loading videos:', error);
+  async function fetchContent(containerName) {
+    const response = await fetch(`http://192.168.3.211:3001/${containerName}`);
+    if (!response.ok) {
+      throw new Error(`Failed to load ${containerName}: ${response.statusText}`);
     }
+    const contentData = await response.json();
+    return contentData.map(item => ({
+      name: item.name,
+      fullPath: item.path
+    }));
+  }
+
+  function findRelatedFile(videoName, fileList) {
+    const relatedFile = fileList.find(file => file.name === videoName);
+    return relatedFile ? relatedFile.fullPath : '';
   }
 
   function openVideoPopup(videoUrl) {
-    const videoName = getVideoName(videoUrl);
     setShowVideoPopup(true);
     setPopupVideoUrl(videoUrl);
   }
@@ -33,12 +61,6 @@ function App() {
     setPopupVideoUrl('');
   }
 
-  function getVideoName(videoUrl) {
-    const videoId = videoUrl.split('/').pop();
-    const video = videos.find(video => video.path.includes(videoId));
-    return video ? video.name : 'Unknown';
-  }
-
   function toggleDarkMode() {
     const newDarkMode = !darkMode;
     setDarkMode(newDarkMode);
@@ -46,44 +68,51 @@ function App() {
   }
 
   return (
-    <div className={`${styles.App} ${darkMode ? styles['dark-mode'] : ''}`}>
-      <div className={styles['video-container']}>
-        {videos.map((video, index) => (
-          <div className={`${styles['video-item']} ${darkMode ? styles['dark-mode'] : ''}`} key={index}>
-            <img
-              className={styles['video-thumbnail']}
-              src={video.thumbnail}
-              alt={video.name}
-              onClick={() => openVideoPopup(video.path)}
-              onMouseOver={(e) => e.target.src = video.gif}
-              onMouseOut={(e) => e.target.src = video.thumbnail}
-            />
-            <div className={styles['video-info']}>
-              <h2 className={styles['video-title']}>{video.name}</h2>
-              <p className={styles['video-description']}>{video.description}</p>
+    <div className={`${styles.app} ${darkMode ? styles['dark-mode'] : ''}`}>
+      {loading && <p>Loading...</p>}
+      {error && <p className={styles.error}>{error}</p>}
+      {!loading && !error && (
+        <>
+          <div className={styles['video-container']}>
+            {videos.map((video, index) => (
+              <div className={`${styles['video-item']} ${darkMode ? styles['dark-mode'] : ''}`} key={index}>
+                <img
+                  className={styles['video-thumbnail']}
+                  src={findRelatedFile(video.name, thumbnails) || defaultThumbnail}
+                  alt={`Thumbnail ${index}`}
+                  onClick={() => openVideoPopup(video.fullPath)}
+                  onMouseOver={(e) => e.target.src = findRelatedFile(video.name, gifs) || defaultGif}
+                  onMouseOut={(e) => e.target.src = findRelatedFile(video.name, thumbnails) || defaultThumbnail}
+                />
+                <div className={styles['video-info']}>
+                  <h2 className={styles['video-title']}>{video.name}</h2>
+                  {/* Adicione descrição se disponível */}
+                  {/* <p className={styles['video-description']}>{videos[index].description}</p> */}
+                </div>
+              </div>
+            ))}
+          </div>
+        
+          {showVideoPopup && (
+            <div className={styles['video-overlay']} onClick={closeVideoPopup}>
+              <div className={styles['video-popup']}>
+                <video className={styles['popup-video']} controls autoPlay src={popupVideoUrl} />
+                {/* <h2 className={styles['video-name']}>{getVideoName(popupVideoUrl)}</h2> */}
+                <span className={styles['close-btn']} onClick={closeVideoPopup}>X</span>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-  
-      {showVideoPopup && (
-        <div className={styles['video-overlay']} onClick={closeVideoPopup}>
-          <div className={styles['video-popup']}>
-            <video className={styles['popup-video']} controls autoPlay src={popupVideoUrl} />
-            <h2 className={styles['video-name']}>{getVideoName(popupVideoUrl)}</h2>
-            <span className={styles['close-btn']} onClick={closeVideoPopup}>X</span>
-          </div>
-        </div>
+          )}
+
+          <button 
+            id="dark-mode-toggle" 
+            className={`${styles['dark-mode-toggle']} ${darkMode ? styles['dark-mode'] : ''}`} 
+            onClick={toggleDarkMode}
+            style={{ backgroundColor: darkMode ? '#fff' : '#26282B', color: darkMode ? '#26282B' : '#fff' }}
+          >
+            {darkMode ? '☀️' : '🌙'}
+          </button>
+        </>
       )}
-  
-      <button 
-          id="dark-mode-toggle" 
-          className={`${styles['dark-mode-toggle']} ${darkMode ? styles['dark-mode'] : ''}`} 
-          onClick={toggleDarkMode}
-          style={{ backgroundColor: darkMode ? '#fff' : '#26282B', color: darkMode ? '#26282B' : '#fff' }}
-      >
-          {darkMode ? '☀️' : '🌙'}
-      </button>
     </div>
   );  
 }
